8f235c037a0c215be4c64c7a26ac2f507106acfe
unknown <pebuka351@gmail.com>
2024-08-24 07:15:59 +0100

    Build

--- src/components/application/custom-checkbox.tsx
+++ src/components/application/custom-checkbox.tsx
@@ -3,7 +3,7 @@ import { Check } from "lucide-react";
 export default function CustomCheckBox({ checked }: { checked: boolean }) {
   return (
     <div
-      className={`large:w-6 flex justify-center items-center large:h-6 w-5 h-5 rounded border border-tremor-content-asYellow ${
+      className={`large:w-6 flex justify-center items-center tablet:w-4 tablet:h-4 large:h-6 w-5 h-5 rounded border border-tremor-content-asYellow ${
         checked ? "bg-tremor-content-asYellow" : "bg-transparent"
       }`}
     >
